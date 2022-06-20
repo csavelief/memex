ff8e80c55f9d4c4364333a7ab1de9922b0cf9363
csavelief <csavelief@computablefacts.com>
2022-06-20 02:26:54 +0200

    Fix script

--- nb.sh
+++ nb.sh
@@ -17,6 +17,10 @@ readonly MEMEX="/opt/git/memex/memex.html"
 readonly MARKER="{date: '', title: '', text: '', file: ''}"
 readonly DATE=$(date '+%Y-%m-%d %H:%M')
 
+quoteRe() {
+	sed -e 's/[^^]/[&]/g; s/\^/\\^/g; $!a\'$'\n''\\n' <<<"$1" | tr -d '\n';
+}
+
 function quoteSubst() {
   IFS= read -d '' -r < <(sed -e ':a' -e '$!{N;ba' -e '}' -e 's/[&/\]/\\&/g; s/\n/\\&/g' <<<"$1")
   printf %s "${REPLY%$'\n'}"
